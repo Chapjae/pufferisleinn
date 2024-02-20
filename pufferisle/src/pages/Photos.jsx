11b4c9631ed9130fa1a815657e56be53f1490cd4
Jay Liu <jayliu89@gmail.com>
2024-02-19 19:11:30 -0800

    added images to photos page

--- pufferisle/src/pages/Photos.jsx
+++ pufferisle/src/pages/Photos.jsx
@@ -1,9 +1,40 @@
-
+import { useState, useEffect } from 'react';
 
 const Photos = () => {
+  const [amenitiesUrl, setAmenitiesUrl] = useState([]);
 
-  
-  return <></>;
+  useEffect(() => {
+    const importAmenities = async () => {
+      try {
+        const amenities = import.meta.glob('../images/amenities/*jpg');
+        const amenitiesPath = Object.keys(amenities);
+        const urls = await Promise.all(
+          amenitiesPath.map(async (path) => {
+            const module = await amenities[path]();
+            return module.default;
+          }),
+        );
+        setAmenitiesUrl(urls);
+      } catch (error) {
+        console.error('Error importing amenties:', error);
+      }
+    };
+    importAmenities();
+  }, []);
+
+  return (
+    <>
+      {amenitiesUrl.length > 0 ? (
+        <>
+          {amenitiesUrl.map((url, index) => (
+            <img src={url} alt={`amenity ${index + 1}`} key={index} />
+          ))}
+        </>
+      ) : (
+        <div>No Pictures Found</div>
+      )}
+    </>
+  );
 };
 
 export default Photos;
