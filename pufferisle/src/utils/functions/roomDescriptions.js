259e84ae14322d7824cd78f0cd1632412093588d
Jay Liu <jayliu89@gmail.com>
2024-02-21 21:52:19 -0800

    fixed issue with descriptions not appearing for room cards

--- pufferisle/src/utils/functions/roomDescriptions.js
+++ pufferisle/src/utils/functions/roomDescriptions.js
@@ -1,14 +1,12 @@
 export const getRoomDescription = (filename) => {
-  const nameWithoutExtension = filename.replace(/\.[^.]+$/, '');
-
-  switch (nameWithoutExtension) {
-    case 'fanroom':
+  switch (filename) {
+    case 'fanroom.jpg':
       return 'Room with fans on the bed';
-    case 'leafroom2':
+    case 'leafroom2.jpg':
       return 'Room with leaves on the bed';
-    case 'moreleaves':
+    case 'moreleaves.jpg':
       return 'Another room with leaves on the bed';
-    case 'tropicalleaf':
+    case 'tropicalleaf.jpg':
       return 'Room with tropical leaves';
     default:
       return 'Description not available';
